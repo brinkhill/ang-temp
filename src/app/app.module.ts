b4c372cba434404ca66f6027cb11b46843e5314c
Brinkley Hill <8brink@gmail.com>
2023-02-08 11:55:45 -0500

    start

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavComponent } from './nav/nav.component';
+import { HeadComponent } from './head/head.component';
+import { SectionComponent } from './section/section.component';
+import { FootComponent } from './foot/foot.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    HeadComponent,
+    SectionComponent,
+    FootComponent
   ],
   imports: [
     BrowserModule,
